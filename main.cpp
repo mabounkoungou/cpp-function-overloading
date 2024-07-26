587b061a4db245802278d2671b88733f14cf6055
raad <raadbouks@gmail.com>
2024-07-26 20:50:18 +0000

    Update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
-//
-int main(){
 
-    return 0;
+int x;
+void Function1(){
+    x = 5;
+    cout << "Inside Function1: x = " << x << endl;
 }
+int main(){
+    
+Function1();
+x = 2;
+cout << "Inside Main Function: x = "<< x << endl;
+
+}
+
+
