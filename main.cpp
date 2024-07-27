cacd24fb278d24d1b7e1d95f501a14a4d8777253
raad <raadbouks@gmail.com>
2024-07-27 15:09:05 +0000

    Create Class and Object

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 using namespace std;
+
+// Want to Create a Class
+class fruits
+{
+    public:
+    string name;
+    int price;
+};
+
+int main()
+{
+    fruits Simple_Fruits;
+    Simple_Fruits.name = "Berry";
+    Simple_Fruits.price = 5;
+
+    fruits Multiple_Fruits;
+    Multiple_Fruits.name = "Banana";
+    Multiple_Fruits.price = 10;
+
+    cout << "Fruit Name: " << Simple_Fruits.name << " " << "and price is " << Simple_Fruits.price << endl;
+    cout << "Fruit Name: " << Multiple_Fruits.name << " " << "and price is " << Multiple_Fruits.price << endl;
+
+    return 0;
+}
+
+/**
 int sum(int k){
     if (k > 0){
         return k + sum(k - 1);
@@ -15,5 +41,7 @@ int main(){
     int result = sum(n);
     cout << "The sum of numbers from 1 to " << n << "is: " << result << endl;
     return 0;
-    
+
 }
+
+*/
